e046a34917958b3be01acb600a956188903691de
Kailuo Wang <kailuo.wang@gmail.com>
2015-11-25 12:12:45 -0500

    updated travis settings

--- project/Testing.scala
+++ project/Testing.scala
@@ -6,7 +6,6 @@ object Testing {
 
 
   lazy val settings = Seq(
-    coverallsToken := Some("tVYHvi1dwcXx3XzTnEOCLjCneOei9wraz"),
     scalacOptions in Test ++= Seq("-Yrangepos"),
     testOptions in Test := Seq(Tests.Argument(TestFrameworks.Specs2, "-xonly"))
   )
