e9bed2a1c783d08617d1406912ee721d3d850be5
Kailuo Wang <kailuo.wang@gmail.com>
2016-08-24 12:48:07 -0400

    fix sbt plugin to recreate swagger.json file rather than writing over it.

--- sbtPlugin/src/main/scala/com/iheart/sbtPlaySwagger/SwaggerPlugin.scala
+++ sbtPlugin/src/main/scala/com/iheart/sbtPlaySwagger/SwaggerPlugin.scala
@@ -30,6 +30,7 @@ object SwaggerPlugin extends AutoPlugin {
     swagger <<= Def.task[File] {
       (target.value / "swagger").mkdirs()
       val file = target.value / "swagger" / "swagger.json"
+      IO.delete(file)
       val args = file.absolutePath +: swaggerDomainNameSpaces.value
       val swaggerClasspath = data((fullClasspath in Runtime).value) ++ update.value.select(configurationFilter(swaggerConfig.name))
       toError(runner.value.run("com.iheart.playSwagger.SwaggerSpecRunner", swaggerClasspath, args, streams.value.log))
