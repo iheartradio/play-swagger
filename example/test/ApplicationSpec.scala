8b4ba94a41dd7bb347e834873c9b73f3de636e2f
Vladimir Grigor <vgrigor@gmail.com>
2017-10-11 16:32:49 +0300

    fix broken route in example (#184)

--- example/test/ApplicationSpec.scala
+++ example/test/ApplicationSpec.scala
@@ -32,9 +32,9 @@ class ApplicationSpec extends PlaySpec with OneAppPerTest {
   "CountController" should {
 
     "return an increasing count" in {
-      contentAsString(route(app, FakeRequest(GET, "/count")).get) mustBe "0"
-      contentAsString(route(app, FakeRequest(GET, "/count")).get) mustBe "1"
-      contentAsString(route(app, FakeRequest(GET, "/count")).get) mustBe "2"
+      contentAsString(route(app, FakeRequest(GET, "/math/count")).get) mustBe "0"
+      contentAsString(route(app, FakeRequest(GET, "/math/count")).get) mustBe "1"
+      contentAsString(route(app, FakeRequest(GET, "/math/count")).get) mustBe "2"
     }
 
   }
