1108631104884fa5d00d81ed460a0e7adb082fee
sullis <github@seansullivan.com>
2018-05-22 18:21:06 -0700

    Play Framework 2.6.13 (#220)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,8 +2,8 @@ import sbt._
 
 object Dependencies {
   object Versions {
-    val play = "2.6.6"
-    val playJson = "2.6.0"
+    val play = "2.6.13"
+    val playJson = "2.6.9"
     val specs2 = "3.8.9"
   }
 
