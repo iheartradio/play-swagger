88db8bc8d774538ae4d9050942171f667fcab79d
Tudor Voicu <tudor.wadv@gmail.com>
2020-06-13 18:06:35 +0200

    Added spec generation support for parametric types.

---
+++ core/src/main/scala/com/iheart/playSwagger/ParametricType.scala
@@ -0,0 +1,44 @@
+package com.iheart.playSwagger
+
+import scala.reflect.runtime.universe._
+import ParametricType._
+
+import scala.collection.immutable.SortedMap
+
+case class ParametricType private (tpe: Type, reifiedTypeName: String, className: String, typeArgsMapping: Map[Line, String]) {
+  val resolve: String ⇒ String = {
+    case ParametricTypeClassName(className, typeArgs) ⇒
+      val resolvedTypes =
+        typeArgs
+          .split(",")
+          .map(_.trim)
+          .map(tn ⇒ typeArgsMapping.getOrElse(tn, resolve(tn)))
+      s"$className[${resolvedTypes.mkString(",")}]"
+    case cn ⇒ typeArgsMapping.getOrElse(cn, cn)
+  }
+}
+
+object ParametricType {
+  final val ParametricTypeClassName = "^(.*?)\\[(.*)\\]$".r
+
+  def apply(reifiedTypeName: String)(implicit cl: ClassLoader): ParametricType = {
+    val mirror = runtimeMirror(cl)
+    reifiedTypeName match {
+      case ParametricTypeClassName(className, typeArgsStr) ⇒
+        val sym = mirror.staticClass(className)
+        val tpe = sym.selfType
+        val typeArgs = typeArgsStr.split(",").map(_.trim).toList
+        val typeArgsMapping = SortedMap(tpe.typeArgs.map(_.toString).zip(typeArgs): _*)
+        ParametricType(tpe, reifiedTypeName, className, typeArgsMapping)
+      case className ⇒
+        val sym = mirror.staticClass(className)
+        val tpe = sym.selfType
+        ParametricType(tpe, className, className, SortedMap.empty)
+    }
+  }
+
+  def apply[T: TypeTag]: ParametricType = {
+    val tpe = implicitly[TypeTag[T]].tpe
+    ParametricType(tpe, tpe.typeSymbol.fullName, tpe.typeSymbol.fullName, Map.empty)
+  }
+}
