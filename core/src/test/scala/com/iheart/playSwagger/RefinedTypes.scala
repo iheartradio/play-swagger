ebeeb368b7452985ca91ecba42ad0d5e07afb52e
Ben Piper <ben@biomatters.com>
2021-01-19 09:22:57 +1300

    Handle non-aliased Scala collection types, and recursively extract type information for more complex refined types (#362).

--- core/src/test/scala/com/iheart/playSwagger/RefinedTypes.scala
+++ core/src/test/scala/com/iheart/playSwagger/RefinedTypes.scala
@@ -3,11 +3,15 @@ package com.iheart.playSwagger
 import eu.timepit.refined._
 import eu.timepit.refined.api._
 import eu.timepit.refined.boolean.And
-import eu.timepit.refined.collection.MinSize
+import eu.timepit.refined.collection.{MinSize, NonEmpty}
 import eu.timepit.refined.numeric._
 import eu.timepit.refined.string._
+
+import scala.collection.immutable
 
 object RefinedTypes {
   type SpotifyAccount = String Refined And[MinSize[W.`6`.T], MatchesRegex[W.`"""@?(\\w){1,15}"""`.T]]
   type Age = Int Refined Positive
+  type NonEmptyList[A] = immutable.List[A] Refined NonEmpty
+  type Albums = NonEmptyList[String]
 }
