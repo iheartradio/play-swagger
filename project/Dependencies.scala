16d658aff4e19dcfb7597f8a5f5ef193167a877a
Scala Steward <scala_steward@virtuslab.com>
2023-01-27 18:43:04 +0000

    Revert commit(s) 4907aaa

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
   object Versions {
     val play = "2.8.18"
-    val playJson = "2.9.4"
+    val playJson = "2.8.2"
     val specs2 = "4.16.1"
     val enumeratum = "1.7.2"
     val refined = "0.10.1"
