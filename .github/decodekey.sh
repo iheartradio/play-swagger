160b9d96487d32ea57434ee23fa9c79af2f0040d
Javakky <javakky@pixiv.co.jp>
2023-07-19 16:53:34 +0900

    build: reset release.yml

--- .github/decodekey.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-
-echo $PGP_SECRET | base64 --decode | gpg --import --no-tty --batch --yes
-
-echo "allow-loopback-pinentry" >>~/.gnupg/gpg-agent.conf
-echo "pinentry-mode loopback" >>~/.gnupg/gpg.conf
-
-gpg-connect-agent reloadagent /bye
