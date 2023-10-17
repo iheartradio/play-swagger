3fac7d183a59a5f837a06df14f3e6d6fd474d67a
Javakky <javakky@pixiv.co.jp>
2023-10-17 17:04:46 +0900

    fix: setup sonatypeSessionName

--- project/Publish.scala
+++ project/Publish.scala
@@ -1,11 +1,12 @@
 import sbt.{Def, _}
 import Keys._
 import com.jsuereth.sbtpgp.PgpKeys.publishSigned
-import xerial.sbt.Sonatype.autoImport.sonatypeProfileName
+import xerial.sbt.Sonatype.autoImport.{sonatypeProfileName, sonatypeSessionName}
 
 object Publish {
 
   val coreSettings: Seq[Def.Setting[_]] = Seq(
+    sonatypeSessionName := s"[sbt-sonatype] ${name.value}-${scalaBinaryVersion.value}-${version.value}",
     organization := "io.github.play-swagger",
     licenses := Seq("Apache-2.0" -> url("https://www.apache.org/licenses/LICENSE-2.0.html")),
     homepage := Some(url("https://github.com/play-swagger/play-swagger")),
