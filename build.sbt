38263365fdd2a4ee9b46efcb77fd729f2ee5d2b1
Javakky <javakky@pixiv.co.jp>
2023-10-17 15:21:50 +0900

    ci: toplevel skip

--- build.sbt
+++ build.sbt
@@ -17,6 +17,7 @@ lazy val scalaV = "2.12.18"
 lazy val root = project.in(file("."))
   .aggregate(playSwagger, sbtPlaySwagger)
   .settings(
+    sonatypeProfileName := "io.github.play-swagger",
     publish / skip := true,
     sourcesInBase := false,
     scalaVersion := scalaV
--- project/Publish.scala
+++ project/Publish.scala
@@ -6,7 +6,6 @@ import xerial.sbt.Sonatype.autoImport.sonatypeProfileName
 object Publish {
 
   val coreSettings: Seq[Def.Setting[_]] = Seq(
-    sonatypeProfileName := "io.github.play-swagger",
     organization := "io.github.play-swagger",
     licenses := Seq("Apache-2.0" -> url("https://www.apache.org/licenses/LICENSE-2.0.html")),
     homepage := Some(url("https://github.com/play-swagger/play-swagger")),
